797e3dfc72f58ec6b96b5bb96c55f67f65c7d2d7
Constant Pagoui <constantpagoui@gmail.com>
2026-01-05 23:36:44 -0500

    Added class clinic

--- src/main/java/com/cpagoui_code/smart_clinic/data/entity/Clinic.java
+++ src/main/java/com/cpagoui_code/smart_clinic/data/entity/Clinic.java
@@ -1,8 +1,47 @@
+
+/*************************************************************************
+ *   Clinic.java                               
+ *   Author : Constant Pagoui                         
+ *   Date   : 2026-01-05                        
+ *                                              
+ *   Summary:                                   
+ *   Represents a medical clinic with contact and location details and the list of       
+ *   associated doctors.                        
+ * **************************************************************************/
+
+
 package com.cpagoui_code.smart_clinic.data.entity;
+import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Clinic {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clinic_id;
+    @NotNull
+    private String clinicName;
+    @NotNull
+    @Size(min = 10, max = 100)
+    private String address;
+    private String phoneNumber;
+
+    private int zipCode;
+
+    @Size(min = 3, max = 100)
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    @OneToMany(mappedBy = "clinic")
+    private List<Doctor> doctors;
 }

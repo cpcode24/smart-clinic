cfff29720e6f70b045dfa1655e4cfe9fd21a197e
Constant Pagoui <constantpagoui@gmail.com>
2026-01-19 21:34:16 -0500

    Tiny changes

--- src/main/java/com/cpagoui_code/smart_clinic/data/repository/PatientRepository.java
+++ src/main/java/com/cpagoui_code/smart_clinic/data/repository/PatientRepository.java
@@ -1,8 +1,10 @@
 package com.cpagoui_code.smart_clinic.data.repository;
-import com.cpagoui_code.smart_clinic.data.entity.Patient;
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cpagoui_code.smart_clinic.data.entity.Patient;
+
 public interface  PatientRepository extends JpaRepository<Patient, UUID>{
-    
+    Patient findPatientById(UUID patientId);
 }
